b8c8016f901a9301c3438959da5f5e24c75c08e7
Davi Torres <torresdavi04@gmail.com>
2024-06-28 15:00:58 -0300

    WIP: VALIDATING GOALS.

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,11 +3,45 @@
 class GoalsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
 
-  def new; end
+  def index;end
 
-  def create; end
+  def new; end
 
   def edit; end
 
-  def update; end
+  def create
+    @goal = Goal.new(goal_params.merge(user: current_user))
+
+    respond_to do |format|
+      if @goal.save
+        format.html do
+          redirect_to goals_path
+        end
+      else
+        format.html do
+          redirect_to goals_path
+        end
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @goal.update(goal_params)
+        format.html do
+          redirect_to root_path
+        end
+      end
+    end
+  end
+
+  private
+
+  def goal
+    @goal ||= Goal.find(params[:id])
+  end
+
+  def goal_params
+    params.permit(:fixed_cost, :comfort, :goals, :pleasures, :financial_freedom, :knowledge)
+  end
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,4 +4,13 @@ class Goal < ApplicationRecord
   belongs_to :user
 
   validates :fixed_cost, :comfort, :goals, :pleasures, :financial_freedom, :knowledge, presence: true
+  validate :validate_sum
+
+  private
+
+  def validate_sum
+    if fixed_cost + comfort + goals + pleasures + financial_freedom + knowledge > 100
+      errors.add(:base, message: 'A soma das metas deve dar 100')
+    end
+  end
 end
