7264a1c9881b788fc96c25e7446766f5ea7242d3
Davi Torres <torresdavi04@gmail.com>
2024-07-02 13:10:54 -0300

    Create sum validation.

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,14 +3,15 @@
 class Goal < ApplicationRecord
   belongs_to :user
 
-  validates :fixed_cost, :comfort, :goals, :pleasures, :financial_freedom, :knowledge, presence: true
+  validates :fixed_cost, :comfort, :goals, :pleasures, :financial_freedom, :knowledge, numericality: { in: 0..100 },
+                                                                                       presence: true
   validate :validate_sum
 
   private
 
   def validate_sum
-    if fixed_cost + comfort + goals + pleasures + financial_freedom + knowledge > 100
-      errors.add(:base, message: 'A soma das metas deve dar 100')
-    end
+    return unless fixed_cost + comfort + goals + pleasures + financial_freedom + knowledge != 100
+
+    errors.add(:base, message: 'A soma das metas deve ser igual a 100.')
   end
 end
