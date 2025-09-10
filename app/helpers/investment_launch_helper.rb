ca68f4081eeeab9229fab60a4eda2882bbcd8bf1
Davi Torres <torresdavi04@gmail.com>
2025-09-09 21:52:12 -0300

    fix: fix `InvestmentLaunchHelper` methods.

--- app/helpers/investment_launch_helper.rb
+++ app/helpers/investment_launch_helper.rb
@@ -2,20 +2,20 @@
 
 module InvestmentLaunchHelper
   def asset_types
-    InvestmentLaunch.asset_types.map do |key, value|
-      [t("activerecord.enums.investment_launch.asset_types.#{key}"), value]
+    InvestmentLaunch.asset_types.keys.map do |key|
+      [t("activerecord.enums.investment_launch.asset_types.#{key}"), key]
     end
   end
 
   def order_types
-    InvestmentLaunch.order_types.map do |key, value|
-      [t("activerecord.enums.investment_launch.order_types.#{key}"), value]
+    InvestmentLaunch.order_types.keys.map do |key|
+      [t("activerecord.enums.investment_launch.order_types.#{key}"), key]
     end
   end
 
   def coin_types
-    InvestmentLaunch.coin_types.map do |key, value|
-      [t("activerecord.enums.investment_launch.coin_types.#{key}"), value]
+    InvestmentLaunch.coin_types.keys.map do |key|
+      [t("activerecord.enums.investment_launch.coin_types.#{key}"), key]
     end
   end
 end
