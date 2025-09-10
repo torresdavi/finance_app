48c487b3635677716b2d570e73e7457541829a13
Davi Torres <torresdavi04@gmail.com>
2025-09-09 21:52:52 -0300

    feat: create `formatter` helpers and create views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include ActiveSupport::NumberHelper
+
+  def format_to_br_date(unformated_date)
+    return unless unformated_date
+
+    unformated_date.strftime('%d/%m/%Y')
+  end
+
+  def format_to_brl(value)
+    return unless value
+
+    number_to_currency(value, unit: 'R$ ', precision: 2, delimiter: '.', separator: ',')
+  end
+
+  def format_to_usd(value)
+    return unless value
+
+    number_to_currency(value, unit: '$ ', precision: 2, delimiter: '.', separator: ',')
+  end
 end
